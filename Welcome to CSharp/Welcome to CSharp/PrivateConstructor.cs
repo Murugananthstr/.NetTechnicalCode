8e953a5cfe32398d4d7ea4e32c98d33eb0cb89e1
Murugananthstr <Ananthsoft@gmail.com>
2017-03-12 02:10:10 +0530

    Private Constructor Demo Check-in

---
+++ Welcome to CSharp/Welcome to CSharp/PrivateConstructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Welcome_to_CSharp
+{
+    class PrivateConstructor
+    {
+        private PrivateConstructor()   { }
+        
+        public static string getData()
+        {
+            return "Private Constructor Demo here";
+        }
+       }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(PrivateConstructor.getData());
+            Console.ReadLine();
+        }
+    }
+}
