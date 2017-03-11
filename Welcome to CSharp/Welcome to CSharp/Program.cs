ad818a81a438f3d5f5be87f5128cdb9cff9258f1
Murugananthstr <Ananthsoft@gmail.com>
2017-03-11 20:19:11 +0530

    Added new Project and removed one existing simple program

---
+++ Welcome to CSharp/Welcome to CSharp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to C#");//to display the output to console/screen
+        Console.ReadLine();// to get some input, hence we will be able to see the above out results 
+    }
+}
