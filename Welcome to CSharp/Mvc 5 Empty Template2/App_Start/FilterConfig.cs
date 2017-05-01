2aa079b67d8266db624607e76b900b2dc9962823
Murugananthstr <Ananthsoft@gmail.com>
2017-05-01 15:28:17 +0530

    Including MVC Projects

---
+++ Welcome to CSharp/Mvc 5 Empty Template2/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mvc_5_Empty_Template2.Infrastructure;
+
+namespace Mvc_5_Empty_Template2.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
