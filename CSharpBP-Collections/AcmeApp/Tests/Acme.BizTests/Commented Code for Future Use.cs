b5dbfb0616f12ef69d6bb453eb36c5ddebce2f94
Muruganantham Devaraj <v-mudeva@microsoft.com>
2017-05-10 20:01:06 +0530

    Commented Code related to AutoMapper

---
+++ CSharpBP-Collections/AcmeApp/Tests/Acme.BizTests/Commented Code for Future Use.cs
@@ -0,0 +1,36 @@
+﻿////Mapper.Initialize(cfg => {
+////    cfg.CreateMap<Foo, Bar>();
+////    cfg.AddProfile<FooProfile>();
+////});
+
+////var config = new MapperConfiguration(cfg => {
+////    cfg.CreateMap<Foo, Bar>();
+////    cfg.AddProfile<FooProfile>();
+////});
+
+////        Mapper.CreateMap<LibraryItem, LibraryItemModel>()
+////.ForMember(
+////    dest => dest.Categories,
+////    opt => opt.MapFrom(src => src.LibraryItemCategories));
+
+
+////Mapper.Initialize(
+////    cfg => cfg.CreateMap<ResourceDataModel.RMScheduleContactRequest, SCMDataModels.ScheduleContactRequest>()
+////           .ForMember(dest => dest.ContactInfo, opt => opt.MapFrom(src => src.ContactInfo)));
+
+////Mapper.Initialize( cfg => cfg.CreateMap<ResourceDataModel.RMScheduleContactRequest, SCMDataModels.ScheduleContactRequest>());
+
+////cfg.CreateMap<InnerSource, InnerDest>();
+
+//// SCMDataModels.ScheduleContactRequest dto = Mapper.Map<ScheduleContactRequest>(scheduleContactRequest);
+////config.AssertConfigurationIsValid();
+
+
+//var config = new MapperConfiguration(cfg => {
+//    cfg.CreateMap<ResourceDataModel.RMScheduleContactRequest, SCMDataModels.ScheduleContactRequest>();
+//    cfg.CreateMap<ResourceDataModel.ContactInformation, SCMDataModels.ContactInformation>();
+//    cfg.CreateMap<ResourceDataModel.CustomerSite, SCMDataModels.CustomerSite>();
+//});
+
+//var mapper = config.CreateMapper();
+//SCMDataModels.ScheduleContactRequest dest = mapper.Map<ResourceDataModel.RMScheduleContactRequest, SCMDataModels.ScheduleContactRequest>(scheduleContactRequest);
