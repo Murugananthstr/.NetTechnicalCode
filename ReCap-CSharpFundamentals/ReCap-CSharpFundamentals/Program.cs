897eb70f8655226578b24d5bbcf2d1ef87641040
Murugananthstr <Ananthsoft@gmail.com>
2018-01-15 14:51:01 +0530

    MultiCast Delegate Changes is checked in for our Understanding purpose

--- ReCap-CSharpFundamentals/ReCap-CSharpFundamentals/Program.cs
+++ ReCap-CSharpFundamentals/ReCap-CSharpFundamentals/Program.cs
@@ -15,14 +15,22 @@ namespace ReCap_CSharpFundamentals
             cus.CustomerAddress = "Ananth";
          
 
-            cus.NameChangeDelegatePointer = new NameChangedDelegateReference(OnNameChange);
-            cus.CustomerAddress = "Muruganantham";
+            cus.NameChangeDelegatePointer = new NameChangedDelegateReference(OnNameChange);//oldway of declration
+            cus.NameChangeDelegatePointer += OnNameChange1;//Multicast Delegate
+            cus.CustomerAddress = "Ananth1";
 
 
             Console.WriteLine("Invoked Customer Method");
             Console.ReadLine();
                 
            }
+        
+        private static void OnNameChange1(string oldNameValue, string newNameValue)
+        {
+            Console.WriteLine("Old Value of Customer Address : {0}", oldNameValue);
+            Console.WriteLine("New Value of Customer Address : {0}", newNameValue);
+
+        }
 
         private static void OnNameChange(string oldNameValue, string newNameValue)
         {
